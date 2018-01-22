55b6f4a50fbe3c39824c0818c57786d490afd1f9
pearce93 <pearce93@hotmail.co.uk>
2018-01-22 17:21:08 +0000

    removing the twitter api error

--- scripts/js/custom.js
+++ scripts/js/custom.js
@@ -195,14 +195,6 @@ jQuery(document).ready(function($){
 
 jQuery(document).ready(function($){
 	
-	$.getJSON('http://api.twitter.com/1/statuses/user_timeline/lukaszholeczek.json?count=3&callback=?', function(tweets){
-		$("#twitter").html(tz_format_twitter(tweets));
-	}); 
-
-});
-
-jQuery(document).ready(function($){
-	
 	/* ------------------- Fancybox --------------------- */
 
 	(function() {
