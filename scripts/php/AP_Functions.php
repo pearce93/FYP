b2af7fb0506c29b57def19788b85f0f65e5f9942
pearce93 <pearce93@hotmail.co.uk>
2018-02-20 16:14:05 +0000

    sse and code tidy

--- scripts/php/AP_Functions.php
+++ scripts/php/AP_Functions.php
@@ -194,8 +194,7 @@
 			<!-- end: Mobile Specific -->
 			
 		    <!-- start: CSS -->
-		    <link rel='stylesheet' type='text/css' media='all'
-      href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css'/>
+		    <link rel='stylesheet' type='text/css' media='all' href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css'/>
 			<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 		    <link href='css/bootstrap.min.css' rel='stylesheet'>
 		    <link href='css/bootstrap-responsive.min.css' rel='stylesheet'>
--- sseArena.php
+++ sseArena.php
@@ -20,27 +20,14 @@
 			<div class="carPark container topMargin">
 				<div class="col-lg-12">
 					<div class="row">
-						<div class="col-md-8 col-xs-12 registrationBox">
-							<h2 class="topMargin" style="text-align: center;">Choose Parking Space</h2>
-							<hr />
-							<?php getCarPark('2'); ?>
-						</div>
-						<div class="col-md-3 col-md-offset-1 col-xs-12 registrationBox">
+						<div class="col-md-3 col-xs-12 registrationBox">
 							<h2 class="topMargin" style="text-align: center;">Details</h2>
 							<hr />
 
 							<div class="row">
 								<div class="col-xs-12 col-md-10 col-md-offset-1">
 									<h2>Arrival:</h2>
-									<input class="form-control" id="arrival" name="arrival" />
-									<script type="text/javascript">
-										$(function(){
-											$("#arrival").datetimepicker({
-												step: 10,
-												minDate: 0,
-											});
-										});
-									</script>									
+									<input class="form-control" id="arrival" name="arrival" />								
 								</div>
 							</div>
 							<div class="row">
@@ -49,14 +36,6 @@
 									<div class="form-group">
 										<input class="form-control" id="departure" name="departure" />
 									</div>
-									<script type="text/javascript">
-										$(function(){
-											$("#departure").datetimepicker({
-												step: 10,
-												minDate: 0,
-											});
-										});
-									</script>									
 								</div>
 							</div>
 							<div class="row">
@@ -68,8 +47,13 @@
 								</div>
 							</div>
 							<div class="topMargin">&nbsp</div>
-
 						</div>
+
+						<div class="col-md-8 col-md-offset-1 col-xs-12 registrationBox">
+							<h2 class="topMargin" style="text-align: center;">Choose Parking Space</h2>
+							<hr />
+							<?php getCarPark('2'); ?>
+						</div>						
 					</div>
 				</div>
 			</div>
